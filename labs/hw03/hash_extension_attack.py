9d9144865d5a9aaa7b81982e18dd8ee84c507c15
Ahnaf Sakeeb Hossain <hossa171@IITS-B473-36.utsc-labs.utoronto.ca>
2017-10-03 20:33:02 -0400

    working on hw03

---
+++ labs/hw03/hash_extension_attack.py
@@ -0,0 +1,55 @@
+import httplib, urlparse, urllib
+from md5p import md5, padding
+
+###############
+### attack ####
+###############
+
+def attack(url, tag, sid, mark): 
+    # parameter url is the attack url you construct
+    parsedURL = urlparse.urlparse(url)
+    #print parsedURL.path
+
+    # open a connection to the server
+    httpconn = httplib.HTTPConnection(parsedURL.hostname, parsedURL.port)
+
+    h = md5(state=tag.decode("hex"),count=512)
+    h.update("&mark=100")
+    illegalTag = h.hexdigest()
+
+    query = parsedURL.path + "?tag=" + illegalTag + "&sid=" + sid + "&mark=" + str(mark); 
+
+    # issue server-API request
+    httpconn.request("GET", query)
+
+    # httpresp is response object containing a status value and possible message
+    httpresp = httpconn.getresponse()
+
+    # valid request will result in httpresp.status value 200
+    print httpresp.status
+
+    # in the case of a valid request, print the server's message
+    print httpresp.read()
+    
+    # return the url that made the attack successul 
+    return(query) # dummy code
+
+
+#############
+### main ####
+#############
+
+if __name__ == "__main__":
+    url = "http://grades.cms-weblab.utsc.utoronto.ca/"
+    tag = "384748b103f0901260027373ebbba319"
+    sid = "1001537483"
+    mark = "100"
+    attack(url, tag, sid, mark)
+    #print(attack(url, tag, sid, mark))
+
+"""
+
+the second url basically generate the MAC for the url:
+/?sid=sid
+
+"""
